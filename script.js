b42d280f3ba747759ef44eecdf9d371ad2f31848
Teja-29-max <vinnakotateja83@gmail.com>
2025-06-25 23:16:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional: Add dynamic task or interaction logic here
+console.log('React-like project initialized with HTML/CSS/JS');
+
+// You could later fetch data and render cards dynamically
